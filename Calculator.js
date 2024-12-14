4fb0a6e7a957fcbd9ef1d6cf46d9f1c254cfacf7
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 21:17:06 -0600

    Calculator working except for decimal button. No tests for proper usage.

--- Calculator.js
+++ Calculator.js
@@ -6,25 +6,56 @@ function createButton(buttonLabel, className, callback) {
     newButton.addEventListener("click", callback);
     return newButton
 }
-function clearScreen (){
+function clear (){
     displayNum.textContent = "";
+    displayExpression.textContent = "";
+    num1 = "";
+    num2 = "";
 }
 
 function appendScreen(text){
     displayNum.textContent = displayNum.textContent += text;
 }
 
+add = (num1, num2) => +num1 + +num2;
+sub = (num1, num2) => +num1 - num2;
+mul = (num1, num2) => +num1 * +num2;
+div = (num1, num2) => +num1 / num2;
+
+function equals(){
+    num2 = displayNum.textContent;
+    switch (operation){
+        case "+":
+            solution = add(num1,num2);
+            break;
+        case "-":
+            solution = sub(num1,num2);
+            break;
+        case "x":
+            solution = mul(num1,num2);
+            break;
+        case "÷":
+            solution = mul(num1,num2);
+            break;
+    }
+    displayNum.textContent = solution;
+    displayExpression.textContent = displayExpression.textContent + +num2 + " = ";
+}
+
 function opFunction(e){
-    num1 = +displayNum.textContent;
-    clearScreen()
+    num1 = displayNum.textContent;
     operation = e.target.textContent;
+    displayNum.textContent = "";
+    displayExpression.textContent = +num1 + ' ' + operation + ' ';
 }
 
 const buttonsBox = document.querySelector("#buttons");
 const displayNum = document.querySelector("#displayNum")
-let num1,num2,operation;
-buttonsBox.appendChild(createButton("Clear", "square", clearScreen));
-buttonsBox.appendChild(createButton("=", "square"));
+const displayExpression = document.querySelector("#displayExpression")
+let num1,num2,operation,solution;
+let num1Filled = false
+buttonsBox.appendChild(createButton("Clear", "square", clear));
+buttonsBox.appendChild(createButton("=", "square", equals));
 for(let i = 0; i < 10; i++){
     buttonsBox.appendChild(createButton(i,"square", (e) => appendScreen(e.target.textContent)));
 }
@@ -33,3 +64,4 @@ buttonsBox.appendChild(createButton("-", "square", opFunction));
 buttonsBox.appendChild(createButton("x", "square", opFunction));
 buttonsBox.appendChild(createButton("÷", "square", opFunction));
 
+buttonsBox.appendChild(createButton(".", "square", (e) => appendScreen(".")));
