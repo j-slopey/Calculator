bb402fa6240e3da21925e03cb8005c5094317fc9
James Slopey <181002700+j-slopey@users.noreply.github.com>
2024-12-13 19:22:47 -0600

    Basic layout created. No operation buttons yet

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+
+function createButton(buttonLabel, className) {
+    const newButton = document.createElement("button");
+    newButton.classList.add(className);
+    newButton.textContent = buttonLabel;
+    return newButton
+}
+
+const buttonsBox = document.querySelector("#buttons");
+for(let i = 0; i < 10; i++){
+    buttonsBox.appendChild(createButton(i,"square"));
+}
+
+
